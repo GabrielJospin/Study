f76768e6bc995f47333b8511209770e286d66f5e
Gabriel Jospin <gabi.jospin@gmail.com>
2020-03-12 17:56:12 -0300

    YourName

---
+++ YourName.java
@@ -0,0 +1,7 @@
+public class YourName {
+	public static void main (String[] args) {
+		String name = args[0];
+		System.out.println("What's your name? ");
+		System.out.println("Hello " + name + ", how are u?");	
+	}
+}
