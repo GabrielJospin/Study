6552edd3a80654e577de0e56b3186ecd39d76198
Gabriel Jospin <gabi.jospin@gmail.com>
2020-04-09 20:57:07 -0300

    Calc

---
+++ calc.java
@@ -0,0 +1,28 @@
+import java.util.Scanner
+public class calc{
+	static void tutorial(){
+		System.out.println("welcome to the tutoria");
+		System.out.println("You post the function and next the number, example: ");
+		System.out.println("SUM");
+		System.out.println("2 3 5 11")
+		System.out.println("21");
+	}
+	
+	static String function(){
+	Scanner input = new.Scanner(System.in);	
+	System.out.print("digite a função a ser utilizada");
+	String func = input.nextLine();
+	Int[] num = 
+
+
+	}
+
+	public static void main(String[] args) {
+	Scanner input = new.Scanner(System.in);
+	System.out.println("Hi, welcome to the Super Calc!");
+	System.out.println("you need a tutorial?, 0 to not and 1 to yes");
+	Interger test = input.nextInt();
+	if (test == 1) tutorial();
+	else (test == 0) function();	
+	}
+}
